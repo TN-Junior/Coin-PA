6e004f741a405ed2bbdbd3ee9ff6022e12540bee
TN-Junior <juju2004.tary@gmail.com>
2024-10-21 21:28:40 -0300

    dashboard

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,27 +1,56 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import Header from '../Header/Header'
-import Datadisplay from './datadisplay'
-import './dashboard.css'
-
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Header from '../Header/Header';
+import LinePlot from './LinePlot';
+import BarChart from './barchart';
+import axios from 'axios';
+import './dashboard.css';
 
 const Dashboard = () => {
+  const [receitas, setReceitas] = useState([]);
+  const [despesas, setDespesas] = useState([]);
+
+  useEffect(() => {
+    // Fetch receitas
+    axios.get('http://localhost:8080/api/contas/receitas')
+      .then(response => {
+        const receitaValores = response.data.map(item => item.valor);
+        setReceitas(receitaValores);
+      })
+      .catch(error => console.error('Error fetching receitas:', error));
+
+    // Fetch despesas
+    axios.get('http://localhost:8080/api/contas/despesas')
+      .then(response => {
+        const despesaValores = response.data.map(item => item.valor);
+        setDespesas(despesaValores);
+      })
+      .catch(error => console.error('Error fetching despesas:', error));
+  }, []);
+
+  console.log(receitas);
+console.log(despesas);
+
   return (
-    <>
     <div className='ContainerEmpresa'>
       <Header />
-    
-        <div className='segundoContainer'>
+      <div className='segundoContainer'>
         <Sidebar />
-          <Datadisplay/>
+        <div className='chartsContainer'>
+          <div className='receitas'>
+          <h2>Receitas</h2>
+          <LinePlot data={receitas} />
+          <BarChart data={receitas}/>
+          </div>
+          <div className='despesas'>
+          <h2>Despesas</h2>
+          <LinePlot data={despesas} />
+          <BarChart data={despesas}/>
+          </div>
         </div>
-        
+      </div>
     </div>
-        
-       
-        
-    </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
